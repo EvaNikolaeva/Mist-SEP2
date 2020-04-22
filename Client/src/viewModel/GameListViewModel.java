3f42dc73b05ce5e19cb744361a85f40619ad1f3f
Artis <mundurs.artis@gmail.com>
2020-04-22 11:12:40 +0200

    Re-re-did the changes in GameListVM

--- Client/src/viewModel/GameListViewModel.java
+++ Client/src/viewModel/GameListViewModel.java
@@ -6,6 +6,8 @@ import model.Game;
 import model.GameList;
 import model.Model;
 
+import java.rmi.RemoteException;
+
 public class GameListViewModel
 {
   private Model model;
@@ -17,8 +19,12 @@ public class GameListViewModel
     this.list = FXCollections.observableArrayList();
   }
 
-  public ObservableList<Game> getList()
-  {
+  public ObservableList<Game> getList() throws RemoteException {
+    GameList games = model.GetGameList();
+    for (int i = 0; i < games.size(); i++)
+    {
+      list.add(games.getGame(i));
+    }
     return list;
   }
 
