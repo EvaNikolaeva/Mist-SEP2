c4a57ac48fc60d2b612211ce26711cb2a7a814c9
Catalin Alexandru <alex_catalin1700@yahoo.com>
2020-05-26 17:03:38 +0200

    zzzzzz

--- Client/src/view/GameListController.java
+++ Client/src/view/GameListController.java
@@ -11,77 +11,102 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.sql.SQLException;
 
-public class GameListController {
+public class GameListController
+{
 
-    @FXML
-    ListView<Game> availableGames;
-    private Region root;
-    private ViewHandler viewHandler;
-    private GameListViewModel gameListViewModel;
+  @FXML ListView<Game> availableGames;
+  @FXML TextField search;
+  private Region root;
+  private ViewHandler viewHandler;
+  private GameListViewModel gameListViewModel;
 
-    public void init(ViewHandler viewHandler, GameListViewModel gameListViewModel,
-                     Region root) throws RemoteException, SQLException {
-        this.root = root;
-        this.viewHandler = viewHandler;
-        this.gameListViewModel = gameListViewModel;
-        this.availableGames.setItems(gameListViewModel.getAvailableGames());
+  public void init(ViewHandler viewHandler, GameListViewModel gameListViewModel,
+      Region root) throws RemoteException, SQLException
+  {
+    this.search.textProperty().bindBidirectional(gameListViewModel.getSearch());
+    this.root = root;
+    this.viewHandler = viewHandler;
+    this.gameListViewModel = gameListViewModel;
+    this.availableGames.setItems(gameListViewModel.getAvailableGames());
+  }
+
+  public Region getRoot()
+  {
+    return root;
+  }
+
+  public void reset() throws RemoteException, SQLException
+  {
+    this.availableGames.setItems(gameListViewModel.getAvailableGames());
+  }
+
+  @FXML public void onMyProfile()
+      throws RemoteException, InterruptedException, NotBoundException,
+      MalformedURLException, SQLException
+  {
+    viewHandler.openView("profile");
+  }
+
+  @FXML public void onAddGame()
+      throws RemoteException, InterruptedException, NotBoundException,
+      MalformedURLException, SQLException
+  {
+    viewHandler.openView("menu");
+  }
+
+  @FXML public void requestGame() throws RemoteException, SQLException
+  {
+    if (availableGames.getSelectionModel().getSelectedIndex() < 0)
+    {
+      Alert alert = new Alert(Alert.AlertType.ERROR,
+          "You have to select a game.", ButtonType.OK);
+      alert.showAndWait();
+      alert.close();
     }
-
-    public Region getRoot() {
-        return root;
+    else if (!availableGames.getSelectionModel().getSelectedItem()
+        .getAvailable())
+    {
+      Alert alert = new Alert(Alert.AlertType.ERROR,
+          "You have to select a available game", ButtonType.OK);
+      alert.showAndWait();
+      alert.close();
     }
-
-    public void reset() throws RemoteException, SQLException {
-        this.availableGames.setItems(gameListViewModel.getAvailableGames());
+    else
+    {
+      gameListViewModel
+          .setGameBuffer(availableGames.getSelectionModel().getSelectedItem());
+      gameListViewModel
+          .requestTrade(availableGames.getSelectionModel().getSelectedItem());
+      reset();
     }
+  }
 
-    @FXML
-    public void onMyProfile()
-            throws RemoteException, InterruptedException, NotBoundException,
-            MalformedURLException, SQLException {
-        viewHandler.openView("profile");
+  @FXML public void onOtherProfile()
+      throws RemoteException, InterruptedException, NotBoundException,
+      MalformedURLException, SQLException
+  {
+    if (availableGames.getSelectionModel().getSelectedIndex() < 0)
+    {
+      Alert alert = new Alert(Alert.AlertType.ERROR,
+          "You have to select a game.", ButtonType.OK);
+      alert.showAndWait();
+      alert.close();
     }
+    else
+    {
+      gameListViewModel
+          .setGameBuffer(availableGames.getSelectionModel().getSelectedItem());
+      viewHandler.openView("other");
+    }
+  }
 
-    @FXML
-    public void onAddGame()
-            throws RemoteException, InterruptedException, NotBoundException,
-            MalformedURLException, SQLException {
-        viewHandler.openView("menu");
-    }
+  @FXML public void onSearch() throws RemoteException, SQLException
+  {
+    availableGames.setItems(gameListViewModel.getSpecificGames());
+  }
 
-    @FXML
-    public void requestGame() throws RemoteException, SQLException {
-        if (availableGames.getSelectionModel().getSelectedIndex() < 0) {
-            Alert alert = new Alert(Alert.AlertType.ERROR,
-                    "You have to select a game.", ButtonType.OK);
-            alert.showAndWait();
-            alert.close();
-        }
-        else if(!availableGames.getSelectionModel().getSelectedItem().getAvailable()){
-            Alert alert = new Alert(Alert.AlertType.ERROR,
-                    "You have to select a available game", ButtonType.OK);
-            alert.showAndWait();
-            alert.close();
-        }
-        else {
-          gameListViewModel.setGameBuffer(availableGames.getSelectionModel().getSelectedItem());
-            gameListViewModel.requestTrade(availableGames.getSelectionModel().getSelectedItem());
-            reset();
-        }
-    }
-
-    @FXML
-    public void onOtherProfile()
-            throws RemoteException, InterruptedException, NotBoundException,
-            MalformedURLException, SQLException {
-        if (availableGames.getSelectionModel().getSelectedIndex() < 0) {
-            Alert alert = new Alert(Alert.AlertType.ERROR,
-                    "You have to select a game.", ButtonType.OK);
-            alert.showAndWait();
-            alert.close();
-        } else {
-          gameListViewModel.setGameBuffer(availableGames.getSelectionModel().getSelectedItem());
-            viewHandler.openView("other");
-        }
-    }
+  @FXML public void onSort() throws RemoteException, SQLException
+  {
+    availableGames.setItems(gameListViewModel.getSortedGames());
+  }
 }
--- Client/src/viewModel/GameListViewModel.java
+++ Client/src/viewModel/GameListViewModel.java
@@ -1,6 +1,8 @@
 package viewModel;
 
 import javafx.application.Platform;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import model.Game;
@@ -12,80 +14,144 @@ import java.beans.PropertyChangeListener;
 import java.rmi.RemoteException;
 import java.sql.SQLException;
 
-public class GameListViewModel implements PropertyChangeListener {
-    private GameListModel model;
-    private ObservableList<Game> availableGames;
+public class GameListViewModel implements PropertyChangeListener
+{
+  private GameListModel model;
+  private ObservableList<Game> availableGames;
+  private StringProperty search;
 
-    public GameListViewModel(GameListModel model) {
-        this.model = model;
-        this.availableGames = FXCollections.observableArrayList();
-        model.addListener(this);
-    }
+  public GameListViewModel(GameListModel model)
+  {
+    this.model = model;
+    this.availableGames = FXCollections.observableArrayList();
+    this.search = new SimpleStringProperty();
+    model.addListener(this);
+  }
 
-    public ObservableList<Game> getAvailableGames() throws RemoteException, SQLException {
-        GameList gameList = model.getAllGamesFromServer();
-        availableGames.clear();
-        for (int i = 0; i < gameList.size(); i++) {
-            if(!(gameList.getGame(i).getId() == -1)){
-                availableGames.add(gameList.getGame(i));
-            }
-        }
-        GameList gameListUnavailable = model.getAllGamesFromServer();
-        return availableGames;
+  public StringProperty getSearch()
+  {
+    return search;
+  }
+
+  public ObservableList<Game> getAvailableGames()
+      throws RemoteException, SQLException
+  {
+    GameList gameList = model.getAllGamesFromServer();
+    availableGames.clear();
+    for (int i = 0; i < gameList.size(); i++)
+    {
+      if (!(gameList.getGame(i).getId() == -1))
+      {
+        availableGames.add(gameList.getGame(i));
+      }
     }
-    public void addGameToGameList(Game game) throws RemoteException, SQLException {
-        availableGames.add(game);
-    }
-    public void removeGameFromGameList(Game game) throws RemoteException, SQLException {
-for(int i = 0; i<availableGames.size() ; i++){
-    if (availableGames.get(i).getTitle().equals(game.getTitle()) && game.getType().equals(availableGames.get(i).getType()) && availableGames.get(i).getUserId() == game.getUserId()) {
+    return availableGames;
+  }
+
+  public void addGameToGameList(Game game) throws RemoteException, SQLException
+  {
+    availableGames.add(game);
+  }
+
+  public void removeGameFromGameList(Game game)
+      throws RemoteException, SQLException
+  {
+    for (int i = 0; i < availableGames.size(); i++)
+    {
+      if (availableGames.get(i).getTitle().equals(game.getTitle()) && game
+          .getType().equals(availableGames.get(i).getType())
+          && availableGames.get(i).getUserId() == game.getUserId())
+      {
         availableGames.remove(i);
+      }
     }
+  }
+
+  public void requestTrade(Game game) throws RemoteException, SQLException
+  {
+    model.clientRequestGame(
+        model.login(model.getUsername(), model.getPassword()), game);
+  }
+
+  public void setGameBuffer(Game game) throws RemoteException
+  {
+    model.setGameBuffer(game);
+  }
+
+  public void updateGameAvailability(Game game)
+  {
+    for (int i = 0; i < availableGames.size(); i++)
+    {
+      if (availableGames.get(i).getId() == game.getId())
+      {
+        availableGames.remove(i);
+        availableGames.add(game);
+      }
+    }
+  }
+
+  public ObservableList<Game> getSpecificGames()
+      throws RemoteException, SQLException
+  {
+    GameList gameList = model.getAllGamesFromServer();
+    availableGames.clear();
+    for (int i = 0; i < gameList.size(); i++)
+    {
+      if (!(gameList.getGame(i).getId() == -1) && (gameList.getGame(i)
+          .getTitle()).equalsIgnoreCase(search.getValue()))
+      {
+        availableGames.add(gameList.getGame(i));
+      }
+    }
+    return availableGames;
+  }
+
+  public ObservableList<Game> getSortedGames()
+      throws RemoteException, SQLException
+  {
+    GameList gameList = model.getAllGamesFromServer();
+    availableGames.clear();
+    for (int i = 0; i < gameList.size(); i++)
+    {
+      if (!(gameList.getGame(i).getId() == -1))
+      {
+        availableGames.add(gameList.getGame(i));
+      }
+    }
+    return availableGames;
+  }
+
+  @Override public void propertyChange(PropertyChangeEvent evt)
+  {
+    Platform.runLater(() -> {
+      switch (evt.getPropertyName())
+      {
+        case "gameAdded":
+          try
+          {
+            addGameToGameList((Game) evt.getNewValue());
+          }
+          catch (RemoteException | SQLException ex)
+          {
+            ex.printStackTrace();
+          }
+          break;
+        case "gameRemoved":
+          try
+          {
+            removeGameFromGameList((Game) evt.getNewValue());
+            System.out.println("trying to remove");
+          }
+          catch (RemoteException | SQLException e)
+          {
+            e.printStackTrace();
+          }
+          break;
+        case "gameAvailabilityChange":
+          System.out.println("Updating availability");
+          updateGameAvailability((Game) evt.getNewValue());
+      }
+    });
+
+  }
 }
-    }
-
-    public void requestTrade(Game game) throws RemoteException, SQLException {
-        model.clientRequestGame(model.login(model.getUsername(), model.getPassword()), game);
-    }
-
-    public void setGameBuffer(Game game) throws RemoteException {
-        model.setGameBuffer(game);
-    }
-    public void updateGameAvailability(Game game){
-        for(int i = 0; i<availableGames.size() ; i++){
-            if (availableGames.get(i).getId() == game.getId()) {
-                availableGames.remove(i);
-                availableGames.add(game);
-            }
-        }
-    }
-    @Override public void propertyChange(PropertyChangeEvent evt) {
-        Platform.runLater(() -> {
-            switch (evt.getPropertyName()) {
-            case "gameAdded":
-                try {
-                    addGameToGameList((Game) evt.getNewValue());
-                } catch (RemoteException ex) {
-                    ex.printStackTrace();
-                } catch (SQLException ex) {
-                    ex.printStackTrace();
-                }
-                break;
-            case "gameRemoved":
-                try {
-                    removeGameFromGameList((Game) evt.getNewValue());
-                    System.out.println("trying to remove");
-                } catch (RemoteException e) {
-                    e.printStackTrace();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-
-                }
-                break;
-                case "gameAvailabilityChange":
-                    System.out.println("Updating availability");
-                    updateGameAvailability((Game) evt.getNewValue());
-        }});
-
-    }
-    }
